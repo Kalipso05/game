941cc0189bd4d03e5b0b5f6abf61d0526b2ec5f5
Kalipso <kurbanovmurad13@gmail.com>
2025-03-22 13:04:08 +0300

    updated

--- static/js/game.js
+++ static/js/game.js
@@ -4,20 +4,51 @@ const money = document.getElementById("money");
 const lvl = document.getElementById("lvl");
 
 
-let moneyCount = 0;
+let audio = null;
 
-enemy.addEventListener("mousedown", () => {
+startGame();
+
+function startGame(){
+    username.textContent = localStorage.getItem("login");
+    money.textContent = localStorage.getItem("money");
+    lvl.textContent = (localStorage.getItem("lvl") == null) ? 1 : localStorage.getItem("lvl");
+    
+}
+
+function handlePress() {
+    enemy.src = "../images/cat_laught.png";
+    let moneyCount = 0;
     enemy.style.transform = "scale(110%)";
-    if(isNaN(parseInt(localStorage.getItem("tapUp")))) localStorage.setItem("tapUp", "1");
-    moneyCount += parseInt(localStorage.getItem("tapUp"));
-    money.textContent = `Баланс ${moneyCount}`;
-    playSound("puk");
-});
-enemy.addEventListener("mouseup", () =>{
-    enemy.style.transform = "scale(100%)";
-});
 
-function playSound(name){
-    let audio = new Audio(`../sound/${name}.mp3`)
+    if (isNaN(parseInt(localStorage.getItem("tapUp")))) {
+        localStorage.setItem("tapUp", "1");
+    }
+
+    let currentMoney = isNaN(parseInt(money.textContent)) ? 0 : parseInt(money.textContent);
+    moneyCount += parseInt(localStorage.getItem("tapUp")) + currentMoney;
+    
+    money.textContent = moneyCount;
+    localStorage.setItem("money", moneyCount);
+
+    playSound("puk");
+    playSound("cat_sound");
+}
+
+function handleRelease() {
+    enemy.style.transform = "scale(100%)";
+    enemy.src = "../images/cat_idle.png";
+    audio.pause();
+}
+
+// Добавляем обработчики для ПК
+enemy.addEventListener("mousedown", handlePress);
+enemy.addEventListener("mouseup", handleRelease);
+
+// Добавляем обработчики для мобильных устройств
+enemy.addEventListener("touchstart", handlePress);
+enemy.addEventListener("touchend", handleRelease);
+export const playSound = (name) =>{
+    audio = new Audio(`../sound/${name}.mp3`)
     audio.play();
+    
 }
--- static/js/store.js
+++ static/js/store.js
@@ -1,8 +1,13 @@
+import { playSound } from "./game.js";
+
 const btnStore = document.getElementById("store");
 const store = document.querySelector(".store");
 
 const tapUp = document.getElementById("tapUp");
 const lvlUp = document.getElementById("lvlUp");
+
+
+
 
 
 let isOpen = false;
@@ -22,14 +27,54 @@ btnStore.addEventListener("click", () => {
 
 tapUp.addEventListener("click", () => {
 
-    let existingTapUp = parseInt(localStorage.getItem("tapUp"));
+    let money = document.getElementById("money");
+    let currentMoney = isNaN(parseInt(money.textContent)) ? 0 : parseInt(money.textContent);
+    if(currentMoney >= 100){
+        playSound("pay");
+        let existingTapUp = parseInt(localStorage.getItem("tapUp"));
 
-    let count = 0;
+        let count = 0;
+        
+        if(!isNaN(existingTapUp)) count += existingTapUp;
+
+        count += 1;
+
+        localStorage.setItem("tapUp", count)
+
+        currentMoney -= 100;
+        localStorage.setItem("money", currentMoney)
+        money.textContent = currentMoney;
+    }
+    else{
+        playSound("pay_fail");
+        alert("Недостаточно средств");
+    }
+
     
-    if(!isNaN(existingTapUp)) count += existingTapUp;
 
-    count += 1;
+});
 
-    localStorage.setItem("tapUp", count)
+lvlUp.addEventListener("click", () => {
+    
+    let lvl = document.getElementById("lvl");
+    let money = document.getElementById("money");
+    
+    let currentMoney = isNaN(parseInt(money.textContent)) ? 0 : parseInt(money.textContent);
+    if(currentMoney >= 500){
+        playSound("pay");
+        let currentLvl = parseInt(lvl.textContent);   
+        currentLvl++;
+        localStorage.setItem("lvl", currentLvl);
+        lvl.textContent = currentLvl;
 
+        currentMoney -= 500;
+        localStorage.setItem("money", currentMoney)
+        money.textContent = currentMoney;
+    }
+    else{
+        playSound("pay_fail");
+        alert("Недостаточно средств");
+    }
+
+    
 });
